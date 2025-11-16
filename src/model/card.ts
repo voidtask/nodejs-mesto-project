2befef45d72728f0654a768ae7def2f40edd8b6b
voidtask <96192210+voidtask@users.noreply.github.com>
2025-11-16 20:02:51 +0400

    Remove version keys in models

--- src/model/card.ts
+++ src/model/card.ts
@@ -4,32 +4,35 @@ import { CardSchema } from "../schemas/card";
 
 type TCard = z.infer<typeof CardSchema>;
 
-const cardSchema = new mongoose.Schema<TCard>({
-  name: {
-    type: String,
-    required: true,
-    validate: (value: unknown) =>
-      CardSchema.shape.name.safeParse(value).success,
+const cardSchema = new mongoose.Schema<TCard>(
+  {
+    name: {
+      type: String,
+      required: true,
+      validate: (value: unknown) =>
+        CardSchema.shape.name.safeParse(value).success,
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: (value: unknown) =>
+        CardSchema.shape.link.safeParse(value).success,
+    },
+    createdAt: {
+      type: mongoose.Schema.Types.Date,
+      default: Date.now,
+    },
+    likes: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+      default: [],
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
   },
-  link: {
-    type: String,
-    required: true,
-    validate: (value: unknown) =>
-      CardSchema.shape.link.safeParse(value).success,
-  },
-  createdAt: {
-    type: mongoose.Schema.Types.Date,
-    default: Date.now,
-  },
-  likes: {
-    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
-    default: [],
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "user",
-  },
-});
+  { versionKey: false }
+);
 
 export const cardModel = mongoose.model<TCard>("card", cardSchema);
--- src/model/user.ts
+++ src/model/user.ts
@@ -4,26 +4,29 @@ import { UserSchema } from "../schemas/user";
 
 export type TUser = z.infer<typeof UserSchema>;
 
-const userMongooseSchema = new mongoose.Schema<TUser>({
-  name: {
-    type: String,
-    validate: {
-      validator: (value: unknown) =>
-        UserSchema.shape.name.safeParse(value).success,
+const userMongooseSchema = new mongoose.Schema<TUser>(
+  {
+    name: {
+      type: String,
+      validate: {
+        validator: (value: unknown) =>
+          UserSchema.shape.name.safeParse(value).success,
+      },
+    },
+    about: {
+      type: String,
+      validate: (value: unknown) =>
+        UserSchema.shape.about.safeParse(value).success,
+    },
+    avatar: {
+      type: String,
+      validate: {
+        validator: (value: unknown) =>
+          UserSchema.shape.avatar.safeParse(value).success,
+      },
     },
   },
-  about: {
-    type: String,
-    validate: (value: unknown) =>
-      UserSchema.shape.about.safeParse(value).success,
-  },
-  avatar: {
-    type: String,
-    validate: {
-      validator: (value: unknown) =>
-        UserSchema.shape.avatar.safeParse(value).success,
-    },
-  },
-});
+  { versionKey: false }
+);
 
 export const userModel = mongoose.model("user", userMongooseSchema);
